f8d91a8abcbf3fbc5809692544edacc02cef3223
Samardeep Kajal <samk712003@gmail.com>
2026-01-28 21:37:59 +0530

    feat(v0.3): Add it-heading component with typography tokens - Create it-heading component with levels 1-4 support - Extend typography.css with heading size, line-height, and weight tokens - Refactor typography.css with improved organization and comments

---
+++ src/components/it-heading.ts
@@ -0,0 +1,45 @@
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("it-heading")
+export class ItHeading extends LitElement {
+  @property({ type: Number }) level: 1 | 2 | 3 | 4 = 1;
+
+  static styles = css`
+    :host {
+      display: block;
+      font-family: var(--it-font-sans);
+      font-weight: var(--it-heading-weight);
+    }
+
+    h1 {
+      fontsize: var(--it-heading-h1);
+      line-height: var(--it-heading-leading-h1);
+    }
+    h2 {
+      fontsize: var(--it-heading-h2);
+      line-height: var(--it-heading-leading-h2);
+    }
+    h3 {
+      fontsize: var(--it-heading-h3);
+      line-height: var(--it-heading-leading-h3);
+    }
+    h4 {
+      fontsize: var(--it-heading-h4);
+      line-height: var(--it-heading-leading-h4);
+    }
+  `;
+
+  render() {
+    switch (this.level) {
+      case 2:
+        return html`<h2><slot></slot></h2>`;
+      case 3:
+        return html`<h3><slot></slot></h3>`;
+      case 4:
+        return html`<h4><slot></slot></h4>`;
+      default:
+        return html`<h1><slot></slot></h1>`;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./components/it-button";
 export * from "./components/it-text";
+export * from "./components/it-heading";
